aece7e59277775d061ed80d5481155617c22c435
Minh Chien DUONG <chienduong90@gmail.com>
2023-12-22 14:49:57 +0100

    secure communication with https and TSL

--- src/main/java/com/vincent/ecommerce/config/MyAppConfig.java
+++ src/main/java/com/vincent/ecommerce/config/MyAppConfig.java
@@ -18,6 +18,6 @@ public class MyAppConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry cors) {
 
         // set up cors mapping
-        cors.addMapping(basePath + "/**").allowedOrigins("http://localhost:4200");
+        cors.addMapping(basePath + "/**").allowedOrigins(theAllowedOrigins);
     }
 }
